238db6ed65a4d2107a3dee10ea9a5065ca2d4729
Ken Brill <kbrill@star2star.com>
2021-01-24 18:59:04 -0600

    Manifest and Admin Menu Update

--- custom/Extension/modules/Administration/Ext/Administration/sqlViewer.php
+++ custom/Extension/modules/Administration/Ext/Administration/sqlViewer.php
@@ -1,14 +1,16 @@
 <?php
-
-$admin_option_defs = array();
-
-$admin_option_defs['Administration']['sqlViewer'] = array(
-    //Icon name. Available icons are located in ./themes/default/images
-    'Administration',
-    //Link name label
-    'LBL_SQLVIEWER_LINK_NAME',
-    //Link description label
-    'LBL_SQLVIEWER_LINK_DESCRIPTION',
-    //Link URL - For Sidecar modules
-    './index.php?module=Administration&action=sqlViewer'
-);
+$exists = false;
+$count = 0;
+foreach ($admin_group_header as $header) {
+    if ($header[0] == 'LBL_ADMINISTRATION_HOME_TITLE') {
+        $exists = true;
+        $admin_group_header[$count][3]['Administration']['sqlViewer'] = array(
+            'EmailMan',
+            'LBL_SQLVIEWER_LINK_NAME',
+            'LBL_SQLVIEWER_LINK_DESCRIPTION',
+            './index.php?module=Administration&action=sqlViewer'
+        );
+        break;
+    }
+    $count++;
+}
--- manifest.php
+++ manifest.php
@@ -1,18 +1,29 @@
 <?php
 
 $manifest = array(
-    'key' => '05171967',
-    'name' => 'sqlViewer',
-    'description' => 'Formats prepared SQL to be readable',
-    'author' => 'Kenneth Brill [ken.brill@gmail.com]',
-    'version' => '1.0',
-    'is_uninstallable' => true,
-    'published_date' => '01/24/2021 14:15:12',
-    'type' => 'module',
-    'readme' => '',
-    'icon' => 'sqlViewer.png',
-    'remove_tables' => '',
-    'uninstall_before_upgrade' => false,
+    'key'                       => '05171967',
+    'name'                      => 'sqlViewer',
+    'acceptable_sugar_flavors'  => array(
+        0 => 'CE',
+        1 => 'PRO',
+        2 => 'ENT',
+        3 => 'DEV'
+    ),
+    'acceptable_sugar_versions' => array(
+        'regex_matches' => array(
+            0 => "^(\d+\.)?(\d+\.)?(\*|\d+)$"
+        ),
+    ),
+    'description'               => 'Formats prepared SQL to be readable',
+    'author'                    => 'Kenneth Brill [ken.brill@gmail.com]',
+    'version'                   => '1.0',
+    'is_uninstallable'          => true,
+    'published_date'            => '01/24/2021 14:15:12',
+    'type'                      => 'module',
+    'readme'                    => '',
+    'icon'                      => 'sqlViewer.png',
+    'remove_tables'             => '',
+    'uninstall_before_upgrade'  => false,
 );
 
 $installdefs = array(
